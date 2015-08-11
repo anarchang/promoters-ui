d231f0f6daf8dcffe38cb02147088e061ec3813e
Ana Chang <anachang@Ana-Chang.local>
2015-08-11 09:33:06 -0600

    Added simple chart

--- app/controllers/scores.js
+++ app/controllers/scores.js
@@ -4,4 +4,6 @@ export default Ember.Controller.extend({
 
     sortProps: ['date:desc'],
     sortedComments: Ember.computed.sort('model', 'sortProps'),
+
+    barheight: function() {this.get('model').score * 10}.property('model')
 });
